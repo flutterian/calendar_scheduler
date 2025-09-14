ab7f1ebab31528bc8126318d31bbd0f07d0be18f
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-09-15 06:59:19 +0900

    [Table calendar] Safe area 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,25 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: TableCalendar(
-          focusedDay: DateTime.now(),
-          firstDay: DateTime(1990, 1, 1),
-          lastDay: DateTime(2999, 12, 31),
-          selectedDayPredicate: (DateTime day) {
-            final now = DateTime.now();
-            return DateTime(
-              day.year,
-              day.month,
-              day.day,
-            ).isAtSameMomentAs(DateTime(now.year, now.month, now.day));
-          },
-          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {},
-          onPageChanged: (DateTime focusedDay) {},
-          rangeSelectionMode: RangeSelectionMode.toggledOff,
-          onRangeSelected:
-              (DateTime? start, DateTime? end, DateTime focusedDay) {},
+        body: SafeArea(
+          child: TableCalendar(
+            focusedDay: DateTime.now(),
+            firstDay: DateTime(1990, 1, 1),
+            lastDay: DateTime(2999, 12, 31),
+            selectedDayPredicate: (DateTime day) {
+              final now = DateTime.now();
+              return DateTime(
+                day.year,
+                day.month,
+                day.day,
+              ).isAtSameMomentAs(DateTime(now.year, now.month, now.day));
+            },
+            onDaySelected: (DateTime selectedDay, DateTime focusedDay) {},
+            onPageChanged: (DateTime focusedDay) {},
+            rangeSelectionMode: RangeSelectionMode.toggledOff,
+            onRangeSelected:
+                (DateTime? start, DateTime? end, DateTime focusedDay) {},
+          ),
         ),
       ),
     ),
