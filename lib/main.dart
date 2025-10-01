9544b17bb582fe553a291b02c3a90dfb690b20d7
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-10-01 21:15:24 +0900

    [프로젝트 초기화] home screen 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
+import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: TableCalendar(
-            focusedDay: DateTime.now(),
-            firstDay: DateTime(1990, 1, 1),
-            lastDay: DateTime(2999, 12, 31),
-            selectedDayPredicate: (DateTime day) {
-              final now = DateTime.now();
-              return DateTime(
-                day.year,
-                day.month,
-                day.day,
-              ).isAtSameMomentAs(DateTime(now.year, now.month, now.day));
-            },
-            onDaySelected: (DateTime selectedDay, DateTime focusedDay) {},
-            onPageChanged: (DateTime focusedDay) {},
-            rangeSelectionMode: RangeSelectionMode.toggledOff,
-            onRangeSelected:
-                (DateTime? start, DateTime? end, DateTime focusedDay) {},
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(home: HomeScreen()));
 }
